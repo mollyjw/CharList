ffe2ae44009d1ff82c4a87638c15b4bcaabca896
Molly J. Whitener <mollyjwhitener@WWPROGRAMMING.COM>
2021-04-06 10:30:30 -0500

    added dummy text to home and about pages; styling is so-so

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="about">
-    <v-sheet color="secondary" height="100em" width="100%">
-      <h1>This is an about page</h1>
+    <v-sheet color="secondary" height="100em" width="100%" style="text-align: center">
+      <h1 style="padding-top: 2%; font: xx-large">About Character Queue</h1>
+      <p style="font-size: x-large; margin-top: 4%; margin-bottom: 2%; margin-left: 10%; margin-right: 10%; text-align: justify">
+        Thank you for being a friend
+        Traveled down a road and back again
+        Your heart is true, you're a pal and a confidant
+        I'm not ashamed to say
+        I hope it always will stay this way
+        My hat is off, won't you stand up and take a bow
+        And if you threw a party
+        Invited everyone you knew
+        Well, you would see the biggest gift would be from me
+        And the card attached would say
+        Thank you for being a friend!
+      </p>
+      <p style="font-size: x-large; margin-top: 4%; margin-bottom: 2%; margin-left: 10%; margin-right: 10%; text-align: justify">
+        If it's a car you lack
+        I'd surely buy you a Cadillac
+        Whatever you need, any time of the day or night
+        I'm not ashamed to say
+        I hope it always will stay this way
+        My hat is off, won't you stand up and take a bow
+        And when we both get older
+        With walking canes and hair of gray
+        Have no fear, even though it's hard to hear
+        I will stand real close and say
+        Thank you for being a friend!
+      </p>
+      <p style="font-size: x-large; margin-top: 4%; margin-bottom: 2%; margin-left: 10%; margin-right: 10%; text-align: justify">Thank you for being a friend</p>
+
     </v-sheet>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
-    <v-sheet color="secondary" height="100em" width="100%">
-      <h1>home page ugh</h1>
+    <v-sheet color="secondary" height="100em" width="100%" style="text-align: center">
+      <h1 style="padding-top: 2%; font: xx-large">Welcome to Character Queue!</h1>
+      <v-img
+        src="https://cdnmetv.metv.com/cpLCu-1469819499-127-quizzes-header.jpg"
+        lazy-src="https://cdnmetv.metv.com/cpLCu-1469819499-127-quizzes-header.jpg"
+        max-width="80%"
+        max-height="auto"
+        style="display: block; margin-left: auto; margin-right: auto; margin-top: 2%"
+      >
+      </v-img>
+      <h2 style="margin-top: 4%; margin-bottom: 2%; margin-left: 10%; margin-right: 10%; text-align: justify">
+        We are lucky to live in a glorious age that gives us everything we could ask for as a human race. What more could you need when you have meat covered in cheese nestled between bread as a complete meal. Salad is essentially food for rabbits, so don’t bother wasting your time.
+      </h2>
+      <h2 style="margin-top: 0%; margin-bottom: 2%; margin-left: 10%; margin-right: 10%; text-align: justify">
+        Any day without a cheeseburger in it is a waste. You can find a cheeseburger no matter where you go in the world, so you have no excuse to miss out on the glory that some people can only dream of. Step up to the plate.
+      </h2>
     </v-sheet>
   </div>
 </template>
